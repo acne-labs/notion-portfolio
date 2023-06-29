0a3d9f48b560b848f7c4bfbac40fa5064baec82b
angry-design <128725715+angry-design@users.noreply.github.com>
2023-06-29 15:30:03 -0300

    Dashed Id site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from "./lib/site-config";
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '38f7fa35e3e64b69a59a90782958b191',
+  rootNotionPageId: '38f7fa35-e3e6-4b69-a59a-90782958b191',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
